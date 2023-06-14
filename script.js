a480e6945427a90c1b4872b3910fbe7ae770b02b
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-13 22:39:35 -0500

    Update to Responsive-Prototype

--- script.js
+++ script.js
@@ -26,12 +26,14 @@
 const boxToCircle = () =>{
     // 1.) target box
     let square = document.querySelector('.box')
-    square.classList.toggle('.circle')
+    square.classList.toggle('circle')
 }
 
 const boxToBio = ()=>{
     let square = document.querySelector('.box')
     square.classList.toggle('bio')
+    let newBox = document.querySelector('.bio')
+    newBox.innerHTML = "This represents "
 }
 
 const showBio = ()=>{
@@ -41,9 +43,31 @@ const showBio = ()=>{
 }
 
 const hideImage = ()=>{
-    let target = document.querySelector('.wondWoman')
+    let target = document.querySelector('.wonderWoman')
     target.classList.toggle('disappear')
     let btn = document.querySelector('.hidden')
     btn.innerHTML = "See Me"
-    wonderWoman.classList.toggle("fighting")
+    wonderWoman.classList.toggle('fighting')
 }
+
+const newImage = () => {
+    let img = document.querySelector('.wondWoman')
+    img.src = 'https://media2.giphy.com/media/ej20RQRslMpAUKr0sI/giphy.gif?cid=6c09b9529mnp4obtu7yrxr2bzb8hgm2g162z37hneldoni8j&ep=v1_gifs_search&rid=giphy.gif&ct=g'
+}
+
+const beepBeep = ()=>{
+    let target = document.querySelector('.roadRunner')
+    target.classList.toggle('spin')
+    let btn = document.querySelector('.hidden')
+    btn.innerHTML = "See Me"
+    wonderWoman.classList.toggle('fighting')
+}
+
+const beginRun = () => {
+    let whiteHorse = document.querySelector('.runningContainer')
+   whiteHorse.classList.toggle('horseRun')
+}
+
+
+
+
